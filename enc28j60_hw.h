8f83b305bab6fa5d997300b864954d36db953a0e
van der Vyver <vyvjohan@784f438f751b.ant.amazon.com>
2018-03-24 20:56:51 +0200

    Update link in enc28j60_hw.h

--- enc28j60_hw.h
+++ enc28j60_hw.h
@@ -1,3 +1,3 @@
 Download from:
 
-https://github.com/opennetworklinux/linux-3.8.13/blob/master/drivers/net/ethernet/microchip/enc28j60_hw.h
+https://raw.githubusercontent.com/torvalds/linux/master/drivers/net/ethernet/microchip/enc28j60_hw.h
